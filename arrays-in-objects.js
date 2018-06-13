14cbbcb14432e85d19adf2ba010da58184120111
MorganHuegel <mhue2910@gmail.com>
2018-06-13 14:48:19 -0400

    arrays-in-objects added

---
+++ arrays-in-objects.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const hobbitMeals = {
+  meals:['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(hobbitMeals.meals[3]);
